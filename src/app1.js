16fb630edf9f915311790e5687918717b50df2d7
wangdisheng <wangdishengyx@163.com>
2022-04-29 01:56:12 +0800

    引入View类，并在C层中初始化V层的数据

--- src/app1.js
+++ src/app1.js
@@ -1,12 +1,13 @@
 import "./app1.css"
 import $ from "jquery"
 import Model from "./base/Model";
+import View from "./base/View";
 
 const eventBus = $(window);
 // 视图相关都放到 m
 const m = new Model({
     data: {
-        n: parseInt(localStorage.getItem("n")) || 100
+        n: parseInt(localStorage.getItem("n") || "100"),
     },
     update(data) {
         Object.assign(m.data, data);
@@ -15,41 +16,40 @@ const m = new Model({
     }
 });
 // 数据相关都放到 v
-const v = {
-    el: undefined,
-    html: `<div>
-        <div class="wrapper">
-            <div class="output">
-                <span id="number">{{n}}</span>
-            </div>
-            <div id="btnCal" class="actions">
-                <button id="add1">+1</button>
-                <button id="minus1">-1</button>
-                <button id="mul2">*2</button>
-                <button id="divide2">÷2</button>
-            </div>
-        </div>
-    </section>`,
-    init(container) {
-        v.el = $(container);
-    },
-    render(n) {
-        // 子元素数
-        if (v.el.children.length !== 0) {
-            v.el.empty(); // 清空
-        }
-        $(v.html.replace("{{n}}", n)).appendTo($(v.el));
-    }
-}
 // 其他的都放到 c
 const c = {
+    v: null,
     ui: undefined, // 不能一开始就初始化ui，因为v.render()没执行之前，是找不到这些元素的
+    initView(){
+        c.v = new View({
+                el: c.container,
+                html: `<div class="wrapper">
+                        <div class="output">
+                            <span id="number">{{n}}</span>
+                        </div>
+                        <div id="btnCal" class="actions">
+                            <button id="add1">+1</button>
+                            <button id="minus1">-1</button>
+                            <button id="mul2">*2</button>
+                            <button id="divide2">÷2</button>
+                        </div>
+                    </div>`,
+                render(n) {
+                    // 子元素数
+                    if (c.v.el.children.length !== 0) {
+                        c.v.el.empty(); // 清空
+                    }
+                    $(c.v.html.replace("{{n}}", n)).appendTo($(c.v.el));
+                }
+            })
+    },
     init(container) {
-        v.init(container);
-        v.render(m.data.n);
+        c.container = container;
+        c.initView();
+        c.v.render(m.data.n);
         c.autoBindEvents();
         eventBus.on("m.data.update", () => {
-            v.render(m.data.n);
+            c.v.render(m.data.n);
         })
     },
     events: {
@@ -62,7 +62,7 @@ const c = {
         for (let key in c.events) {
             const value = c[c.events[key]]; // value是一个方法
             const keys = key.split(" ");
-            v.el.on(keys[0], keys[1], value); // 绑定事件，但是没有重新渲染
+            c.v.el.on(keys[0], keys[1], value); // 绑定事件，但是没有重新渲染
         }
     },
     add() {

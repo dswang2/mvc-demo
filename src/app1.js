10a34faef8c6078f00263d2d9dfc5bfdc544e9be
wangdisheng <wangdishengyx@163.com>
2022-04-29 23:07:12 +0800

    引入View类，作为VC的基类

--- src/app1.js
+++ src/app1.js
@@ -1,6 +1,7 @@
 import "./app1.css"
 import $ from "jquery"
 import Model from "./base/Model";
+import View from "./base/View";
 
 const eventBus = $(window);
 // 视图相关都放到 m
@@ -15,62 +16,52 @@ const m = new Model({
     }
 });
 // 其他的都放到 v c
-const view = {
-    el: undefined,
-    html: `<div>
-        <div class="wrapper">
-            <div class="output">
-                <span id="number">{{n}}</span>
-            </div>
-            <div id="btnCal" class="actions">
-                <button id="add1">+1</button>
-                <button id="minus1">-1</button>
-                <button id="mul2">*2</button>
-                <button id="divide2">÷2</button>
-            </div>
-        </div>
-    </section>`,
-    ui: undefined, // 不能一开始就初始化ui，因为c.render()没执行之前，是找不到这些元素的
-    init(container) {
-        view.el = $(container);
-        view.render(m.data.n);
-        view.autoBindEvents();
-        eventBus.on("m.data.update", () => {
-            view.render(m.data.n);
-        })
-    },
-    render(n) {
-        // 子元素数
-        if (view.el.children.length !== 0) {
-            view.el.empty(); // 清空
-        }
-        $(view.html.replace("{{n}}", n)).appendTo($(view.el));
-    },
-    events: {
-        "click #add1": "add",
-        "click #minus1": "minus",
-        "click #mul2": "mul",
-        "click #divide2": "divide",
-    },
-    autoBindEvents() {
-        for (let key in view.events) {
-            const value = view[view.events[key]]; // value是一个方法
-            const keys = key.split(" ");
-            view.el.on(keys[0], keys[1], value); // 绑定事件，但是没有重新渲染
-        }
-    },
-    add() {
-        m.update({n: m.data.n + 1});
-    },
-    minus() {
-        m.update({n: m.data.n - 1});
-    },
-    mul() {
-        m.update({n: m.data.n * 2});
-    },
-    divide() {
-        m.update({n: m.data.n / 2});
-    },
+let view = null;
+
+function initView(container){
+    view = new View({
+        data: m.data,
+        el: container,
+        html: `<div>
+                <div class="wrapper">
+                    <div class="output">
+                        <span id="number">{{n}}</span>
+                    </div>
+                    <div id="btnCal" class="actions">
+                        <button id="add1">+1</button>
+                        <button id="minus1">-1</button>
+                        <button id="mul2">*2</button>
+                        <button id="divide2">÷2</button>
+                    </div>
+                </div>
+            </section>`,
+        render(data) {
+            // 子元素数
+            if (this.el.children.length !== 0) {
+                this.el.empty(); // 清空
+            }
+            $(this.html.replace("{{n}}", data.n)).appendTo($(this.el));
+        },
+        eventBus: eventBus,
+        events: {
+            "click #add1": "add",
+            "click #minus1": "minus",
+            "click #mul2": "mul",
+            "click #divide2": "divide",
+        },
+        add() {
+            m.update({n: m.data.n + 1});
+        },
+        minus() {
+            m.update({n: m.data.n - 1});
+        },
+        mul() {
+            m.update({n: m.data.n * 2});
+        },
+        divide() {
+            m.update({n: m.data.n / 2});
+        },
+    })
 }
 
-export default view;
+export default initView;
--- src/base/View.js
+++ src/base/View.js
@@ -1,10 +1,23 @@
 import $ from "jquery"
 
 class View {
-    constructor({el, html, render}) {
-        this.el = $(el);
-        this.html = html;
-        this.render = render;
+    constructor(options) {
+        Object.assign(this,options);
+        debugger;
+        this.el = $(this.el);
+        this.render(this.data);
+        this.autoBindEvents();
+        this.eventBus.on("m.data.update", () => {
+            this.render(this.data);
+        });
+    }
+
+    autoBindEvents() {
+        for (let key in this.events) {
+            const value = this[this.events[key]]; // value是一个方法
+            const keys = key.split(" ");
+            this.el.on(keys[0], keys[1], value); // 绑定事件，但是没有重新渲染
+        }
     }
 }
 
